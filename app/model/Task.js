473ef13d8e9ee5bfc910119741171e867613a7a7
vvk0812 <vvk7899@gmail.com>
2024-09-21 14:12:01 +0530

    date commit

--- app/model/Task.js
+++ app/model/Task.js
@@ -12,6 +12,10 @@ var schema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    }
  },
   { timestamps: true });
 
